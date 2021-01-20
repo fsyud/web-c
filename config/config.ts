e85316a14ae3cb12ecc1b09f93ad3d3887f89e2c
dong <dongyu@duliday.com>
2021-01-20 16:55:58 +0800

    add canvas

--- config/config.ts
+++ config/config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'umi';
+import path from 'path';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  hash: true,
+  // 浏览器兼容版本
+  targets: {
+    android: 5,
+    ios: 7,
+    chrome: 58,
+    ie: 9,
+  },
   fastRefresh: {},
   publicPath: '/static/',
-  hash: true,
-  ssr: { mode: 'stream' },
   history: {
-    type: 'browser',
+    type: 'hash',
+  },
+  alias: {
+    '@/': path.resolve(__dirname, '../src/'),
   },
   routes: [{ path: '/', component: '@/pages/index' }],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './index.less';
+import { RibbonsFun } from '@/utils/lib/bg';
 
 export default () => {
+  useEffect(() => {
+    RibbonsFun();
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>Page index1</h1>
     </div>
   );
 };
