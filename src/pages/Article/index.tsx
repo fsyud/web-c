66ddf3e2b1a0cfff5361a3d6754ed9b0d3016b3f
dongyu <naze.dong@soundws.com>
2021-04-28 18:08:32 +0800

    style: box-shadow

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -3,6 +3,7 @@ import { Form, Card } from 'antd';
 import { ConnectProps } from 'umi';
 import StandardFormRow from './components/StandardFormRow';
 import TagSelect from './components/TagSelect';
+import { tyoeArr } from './index';
 import styles from './index.less';
 
 const FormItem = Form.Item;
@@ -13,19 +14,6 @@ interface ArticleProps {
 interface FormDataType {
   tag_ids: (string | number)[];
 }
-
-const tyoeArr = [
-  { v: 1, n: '全部' },
-  { v: 2, n: 'java' },
-  { v: 3, n: '前端' },
-  { v: 4, n: '后端' },
-  { v: 5, n: '工具' },
-  { v: 6, n: 'mac' },
-  { v: 7, n: '算法' },
-  { v: 8, n: 'git' },
-  { v: 9, n: 'mysql' },
-  { v: 10, n: 'react' },
-];
 
 const Article: React.FC<ArticleProps> = (props) => {
   const [formData, setFormData] = useState<FormDataType>({
