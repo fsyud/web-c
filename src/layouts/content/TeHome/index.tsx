0f4f624709f34460d96c1207787db72a05db5396
dongyu <naze.dong@soundws.com>
2021-06-21 20:47:47 +0800

    feat: 首页完善

--- src/layouts/content/TeHome/index.tsx
+++ src/layouts/content/TeHome/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Row, Col, Skeleton } from 'antd';
+import {
+  HighlightOutlined,
+  MessageOutlined,
+  ReadOutlined,
+} from '@ant-design/icons';
 import styles from './index.less';
 
 interface TeHomeProps {
@@ -14,6 +19,26 @@ const TeHome: React.FC<TeHomeProps> = ({ children }) => {
           <section className={styles.init_section}>{children}</section>
         </Col>
         <Col span={6} className={styles.init_aside}>
+          <aside>
+            <div className={styles.tehome_one}>
+              <h5>写下你想说的</h5>
+              <ul>
+                <li>
+                  <HighlightOutlined />
+                  <span>写文章</span>
+                </li>
+                <li>
+                  <MessageOutlined />
+                  <span>发片刻</span>
+                </li>
+                <li>
+                  <ReadOutlined />
+                  <span>写文章</span>
+                </li>
+              </ul>
+            </div>
+            <Skeleton />
+          </aside>
           <aside>
             <Skeleton />
           </aside>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import { Skeleton, Tag, Divider } from 'antd';
+import React, { useState } from 'react';
+import { Skeleton, Divider, Button } from 'antd';
+import classnames from 'classnames';
+import TagSelect from '@/components/Article/TagSelect';
+import ArtList from '@/components/Article/ArtList';
 
 import styles from './index.less';
 
 interface AboutProps {}
 
+const btnConf: string[] = ['本月最热', '全部热门', '最新'];
+
 const About: React.FC<AboutProps> = (props) => {
+  const [btnType, setBtnType] = useState<number>();
+
+  const TypeClick = (pm: number): void => {
+    setBtnType(pm);
+  };
+
+  const btnStyle = (pm: number): string => {
+    return classnames({
+      [styles.active]: btnType === pm,
+    });
+  };
+
   return (
     <div className={styles.home}>
       <div className={styles.h_tags}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((s: any, index: number) => {
+        <TagSelect list={[1, 2, 3, 4, 5, 6, 7, 8, 9]} />
+      </div>
+      <Divider />
+      <div className={styles.h_header__link}>
+        {btnConf.map((s: any, index: number) => {
           return (
-            <Tag key={index} color="blue">
-              测试1
-            </Tag>
+            <Button
+              className={btnStyle(index)}
+              onClick={() => TypeClick(index)}
+              type="link"
+            >
+              {s}
+            </Button>
           );
         })}
-        <Tag>更多...</Tag>
       </div>
+      <Divider />
+
+      <div className={styles.h_main}>
+        <ArtList />
+      </div>
+
+      <Skeleton active />
+
       <Skeleton active />
     </div>
   );
